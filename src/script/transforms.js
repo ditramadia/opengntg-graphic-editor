0ec5bc51589d024ea680b9ce553e360d25a2060d
ditramadia <ditrarizqaamadia@gmail.com>
2024-03-20 14:45:11 +0700

    feat: add move line with cursor

--- src/script/transforms.js
+++ src/script/transforms.js
@@ -33,3 +33,41 @@ translateYInput.addEventListener("input", () => {
     vertexObj.setVertexY(vertexIdx, finalY);
   }
 });
+
+// == Move ================================================================
+// Variables
+let isMoving = false;
+
+// Event handler
+// User has started to move a vertex
+canvas.addEventListener("mousedown", () => {
+  if (!isMoving && isEditing) {
+    isMoving = true;
+  }
+});
+
+// User is moving a vertex
+canvas.addEventListener("mousemove", (e) => {
+  if (!isMoving || !isEditing) {
+    return;
+  }
+
+  const { x, y } = getMousePos(e);
+  for (let i = 0; i < selectedPoints.length; i++) {
+    vertexObj = selectedPoints[i];
+    vertexIdx = selectedPointIndex[i];
+    const initialX = initialPointsPosition[i][0];
+    const initialY = initialPointsPosition[i][1];
+    const finalX = initialX + x;
+    const finalY = initialY + y;
+    vertexObj.setVertexX(vertexIdx, finalX);
+    vertexObj.setVertexY(vertexIdx, finalY);
+  }
+});
+
+// User has stopped moving a vertex
+canvas.addEventListener("mouseup", () => {
+  if (isMoving) {
+    isMoving = false;
+  }
+});
