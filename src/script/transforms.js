eefd19c03c8448cc07782c70bb723c2f9373ec39
ditramadia <ditrarizqaamadia@gmail.com>
2024-03-20 14:39:31 +0700

    feat: add y translation for line

--- src/script/transforms.js
+++ src/script/transforms.js
@@ -1,11 +1,9 @@
 // == Translate X =========================================================
 // Variables
-const translateXValueSpan = document.querySelector("#translate-x-value");
 const translateXInput = document.querySelector("#translate-x-input");
 
 // Event handler
 translateXInput.addEventListener("input", () => {
-  translateXValueSpan.innerHTML = translateXInput.value;
   const xValue = translateXInput.value / 100;
 
   // Translate every selected vertices
@@ -15,5 +13,23 @@ translateXInput.addEventListener("input", () => {
     const initialX = initialPointsPosition[i][0];
     const finalX = initialX + xValue;
     vertexObj.setVertexX(vertexIdx, finalX);
+  }
+});
+
+// == Translate X =========================================================
+// Variables
+const translateYInput = document.querySelector("#translate-y-input");
+
+// Event handler
+translateYInput.addEventListener("input", () => {
+  const yValue = translateYInput.value / 100;
+
+  // Translate every selected vertices
+  for (let i = 0; i < selectedPoints.length; i++) {
+    vertexObj = selectedPoints[i];
+    vertexIdx = selectedPointIndex[i];
+    const initialY = initialPointsPosition[i][1];
+    const finalY = initialY + yValue;
+    vertexObj.setVertexY(vertexIdx, finalY);
   }
 });
